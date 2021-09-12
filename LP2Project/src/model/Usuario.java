a0d067e36e380cd6cae4553f65f40ce08b8c7386
OSCAR DANIEL <>
2021-09-12 11:07:05 -0500

    Hola

---
+++ LP2Project/src/model/Usuario.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ * @author NAVARRO CIEZA, OSCAR DANIEL - 20186008
+ * 
+ */
+public class Usuario {
+
+}
